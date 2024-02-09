c775680d55914acf074ebe22e15a25bce017d54b
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-02-09 20:12:51 -0300

    feat: add zod validation to the env variables

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
+import { env } from '../env';
 
 const app = express();
-const SERVER_PORT = process.env.SERVER_PORT;
+const SERVER_PORT = env.SERVER_PORT;
 
 app.listen(SERVER_PORT, () => console.log(`Server is running :${SERVER_PORT} 🚀`));
